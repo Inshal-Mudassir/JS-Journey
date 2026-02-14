0cc6b227c1e3ee82e7e41ca2f457e8bc7c8cae6f
Inshal <inshalmudassar715@gmail.com>
2026-02-14 17:51:22 +0500

    Practice Set

---
+++ 6_ControlFlow/5_PracticeTest.js
@@ -0,0 +1,33 @@
+// 🧠 JavaScript Complete Code Questions (Beginner Level)
+// 1️⃣ Values & Variables
+
+// Declare a variable name and store your name.
+// Print it.
+
+// let name = "inshal";
+// console.log(name);
+
+// 2️⃣ let vs const
+
+// Declare a constant PI with value 3.14.
+// Try changing it and print the result.
+
+// const PI = 3.14;
+// PI = 2.14;          // Const cannot be updates
+// console.log(PI);
+
+// 3️⃣ Data Types
+
+// Print the type of each value:
+
+// 10
+// "Hello"
+// true
+// null
+// undefined
+
+console.log(typeof(10));
+console.log(typeof("Hello"));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
