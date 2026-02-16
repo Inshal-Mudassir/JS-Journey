858a3729ce32710c2f4910f161ef2d8eb8713388
Inshal <inshalmudassar715@gmail.com>
2026-02-16 21:48:04 +0500

    unary practice

--- 4_Operators/9_practice.js
+++ 4_Operators/9_practice.js
@@ -1,14 +1,62 @@
-let a = 12;
-let b = 7;
+// let a = 12;
+// let b = 7;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a ** b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
 
-a++;
-++a;
-console.log(a);
-console.log(a);
+// a++;
+// ++a;
+// console.log(a);
+// console.log(a);
+
+// let num = 3;
+// num += 2;
+// console.log(num);
+
+// Comparision Operators
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+// console.log(5 != "5");
+// console.log(5 !== 3);
+
+// Logical Operators
+
+// let age = 20;
+// let hasId = true;
+
+// if (age > 18 && hasId) {
+//     console.log("Adult");
+// }
+
+// let isAdmin = false;
+// let isModerator = true;
+
+// if (isAdmin || isModerator) {
+//     console.log("You are elegible.")
+// }
+
+// let isLogin = false;
+
+// if (!isLogin) {
+//     console.log("Please Login!");
+// }
+
+// Unary Operators
+
+// console.log(+"5");
+// console.log(+true);
+// console.log(+false);
+// console.log(+undefined);
+// console.log(+null);
+
+// console.log(-"5");
+// console.log(-true);
+// console.log(-false);
+// console.log(-undefined);
+// console.log(-null);
+
