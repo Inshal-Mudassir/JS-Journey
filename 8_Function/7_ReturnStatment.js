0b5787fe30ff5c0fa54d9602e08bd8070b364e9c
Inshal <inshalmudassar715@gmail.com>
2026-02-22 22:13:21 +0500

    Return Statment Done

---
+++ 8_Function/7_ReturnStatment.js
@@ -0,0 +1,46 @@
+// return Statement — JavaScript (Very Easy 😊)
+// The return statement sends a value back from a function and stops the function.
+
+// 🔹 Simple Example
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let result = add(2, 3);
+// console.log(result); // 5
+// 👉 return a + b sends 5 back to result.
+
+// 🔹 Without return
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// let result = add(2, 3);
+// console.log(result); // undefined
+// 👉 No return → function gives undefined.
+
+// 🔹 Important Rule ⚠️
+// When return runs, the function stops immediately.
+
+// function test() {
+//   return "Hello";
+//   console.log("Bye"); // ❌ never runs
+// }
+// 🔹 Returning Anything
+// You can return:
+
+// Number
+
+// String
+
+// Boolean
+
+// Array
+
+// Object
+
+// function isAdult(age) {
+//   return age >= 18;
+// }
+// 🔑 Easy Line to Remember
+// return gives a value back and ends the function
