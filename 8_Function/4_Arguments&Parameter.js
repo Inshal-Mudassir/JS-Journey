a2edd6f4df140c13ae6f8413ef4493a4b13e2ec4
Inshal <inshalmudassar715@gmail.com>
2026-02-22 21:47:50 +0500

    Arguments And Parameter Done

---
+++ 8_Function/4_Arguments&Parameter.js
@@ -0,0 +1,39 @@
+// Parameters vs Arguments — JavaScript (Very Easy 😊)
+// This topic is super simple once you see the difference 👇
+
+// 🔹 Parameters
+// Parameters are the names listed in the function definition.
+// 👉 They act like placeholders.
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// Here:
+
+// a and b are parameters
+
+// 🔹 Arguments
+// Arguments are the actual values you pass when calling the function.
+
+// add(2, 3);
+// Here:
+
+// 2 and 3 are arguments
+
+// 🔹 Easy Way to Remember 🧠
+// Parameters	Arguments
+// Function side	Calling side
+// Variable names	Real values
+// a, b	2, 3
+// 🔹 Another Simple Example
+// function greet(name) {   // parameter
+//   console.log("Hello " + name);
+// }
+
+// greet("Ali");           // argument
+// name → parameter
+
+// "Ali" → argument
+
+// ⭐ One‑Line Rule
+// Parameters receive values, arguments send values
