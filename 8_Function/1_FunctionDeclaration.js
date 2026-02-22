3a6e9aff6c68f78530edd1436325189edef9be0b
Inshal <inshalmudassar715@gmail.com>
2026-02-22 21:17:42 +0500

    Function Declaration Done

---
+++ 8_Function/1_FunctionDeclaration.js
@@ -0,0 +1,48 @@
+// Function Declaration — JavaScript (Very Easy 😊)
+// A function declaration is a way to create a function with a name that you can use anywhere in your code.
+
+// 🔹 Basic Syntax
+// function functionName() {
+//   // code to run
+// }
+// 🔹 Simple Example
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// sayHello(); // calls the function
+// Output:
+
+// Hello!
+// 🔹 Function with Parameters
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(2, 3); // 5
+// 👉 a and b are parameters (inputs).
+
+// 🔹 Function with Return
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// let result = multiply(4, 5);
+// console.log(result); // 20
+// 👉 return sends a value back.
+
+// ⭐ Important Point (Hoisting)
+// Function declarations are hoisted, so you can call them before they are written:
+
+// greet();
+
+// function greet() {
+//   console.log("Hi");
+// }
+// This works ✅
+
+// 🔹 When to Use
+// ✔ Reuse code
+// ✔ Organize logic
+// ✔ Clean and readable programs
+
---
+++ 8_Function/practice.js
@@ -0,0 +1,15 @@
+// function sayHello () {
+//     console.log("Hello!");
+// }
+
+// sayHello();
+
+// Function With Parameter
+
+// function multiplication (x,y) {
+//     return x * y;
+// }
+
+// let result = multiplication (3,4);
+// console.log(result);
+
