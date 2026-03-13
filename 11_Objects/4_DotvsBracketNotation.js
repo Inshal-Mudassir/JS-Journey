7f9ec391f8d81abc41172607e3b342c585ed50de
Inshal <inshalmudassar715@gmail.com>
2026-03-13 22:04:55 +0500

    Dot Vs Bracket Notation

---
+++ 11_Objects/4_DotvsBracketNotation.js
@@ -0,0 +1,31 @@
+// Dot vs Bracket Notation — JavaScript 😊
+// Both are used to access object properties.
+
+// 🔹 Dot Notation
+// let person = {
+//   name: "Ali",
+//   age: 20
+// };
+
+// console.log(person.name);
+// Output
+
+// Ali
+// 👉 Used when the property name is known.
+
+// 🔹 Bracket Notation
+// let person = {
+//   name: "Ali",
+//   age: 20
+// };
+
+// console.log(person["age"]);
+// Output
+
+// 20
+// 👉 Used when the property name is in a variable or dynamic.
+
+// 🔑 Easy Rule
+// object.name → Dot notation
+
+// object["name"] → Bracket notation
