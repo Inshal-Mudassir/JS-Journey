8000905b223dc22f1737fa6ebb7bfdf797b4321e
Inshal <inshalmudassar715@gmail.com>
2026-03-13 11:01:06 +0500

    Accessing Elements

---
+++ 10_Arrays/2_AccessingElements.js
@@ -0,0 +1,31 @@
+// Accessing Elements — JavaScript Arrays (Very Easy 😊)
+// Accessing elements means getting a value from an array using its index.
+
+// 👉 Array indexes start from 0.
+
+// 🔹 Simple Example
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// console.log(fruits[0]); // Apple
+// console.log(fruits[1]); // Banana
+// console.log(fruits[2]); // Mango
+// 👉 fruits[0] = first element
+// 👉 fruits[1] = second element
+
+// 🔹 Access Last Element
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// console.log(fruits[fruits.length - 1]); // Mango
+// 🔹 Changing an Element
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// fruits[1] = "Orange";
+
+// console.log(fruits); // ["Apple", "Orange", "Mango"]
+// 🔑 Easy Rule
+// Array[index] → used to get or change values
+
+// Example:
+
+// 0   1   2
+// A   B   C
--- 10_Arrays/Practice.js
+++ 10_Arrays/Practice.js
@@ -5,5 +5,12 @@
 // console.log(numbers[1]);
 // console.log(numbers[2]);
 // console.log(numbers[3]);
-let numbers = new Array(4);
-console.log(numbers);
+// let numbers = new Array(4);
+// console.log(numbers);
+
+let num = [1,2,3,4,12];
+num[4] = 32;
+// console.log(num.length);
+// console.log(num[num.length - 1]);
+let IndexOfLastEl = num.length;
+console.log(num[IndexOfLastEl - 1]);
