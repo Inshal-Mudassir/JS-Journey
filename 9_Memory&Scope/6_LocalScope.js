1aea300a3fcb220d521fc32f1f0f738481cdf10f
Inshal <inshalmudassar715@gmail.com>
2026-03-13 10:33:45 +0500

    Local Scope

---
+++ 9_Memory&Scope/6_LocalScope.js
@@ -0,0 +1,27 @@
+// Local Scope — JavaScript (Very Easy 😊)
+// Local scope means a variable is created inside a function, and it can be used only inside that function.
+
+// 🔹 Simple Example
+// function test() {
+//   let x = 10; // local variable
+//   console.log(x);
+// }
+
+// test(); // 10
+// console.log(x); // ❌ Error
+// 👉 x exists only inside test().
+
+// 🔹 Another Example
+// function greet() {
+//   let name = "Ali"; // local
+//   console.log("Hello " + name);
+// }
+
+// greet();
+// Output:
+
+// Hello Ali
+// 👉 name cannot be used outside the function.
+
+// 🔑 Easy Rule
+// Local variables work only inside the function where they are created.
