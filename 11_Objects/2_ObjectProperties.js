087fc62ea5fd1acf2b5b61c225792733b14c7d07
Inshal <inshalmudassar715@gmail.com>
2026-03-13 21:58:28 +0500

    Object Properties

---
+++ 11_Objects/2_ObjectProperties.js
@@ -0,0 +1,32 @@
+// Object Properties — JavaScript (Very Easy 😊)
+// Object properties are the data stored inside an object (key : value).
+
+// 🔹 Example
+// let person = {
+//   name: "Ali",
+//   age: 20,
+//   city: "Lahore"
+// };
+// Here:
+
+// Property	Value
+// name	"Ali"
+// age	20
+// city	"Lahore"
+// 👉 name, age, city are object properties.
+
+// 🔹 Access Properties
+// 1️⃣ Dot Notation
+
+// console.log(person.name);
+// Output
+
+// Ali
+// 2️⃣ Bracket Notation
+
+// console.log(person["age"]);
+// Output
+
+// 20
+// 🔑 Easy Rule
+// Object Property = key + value stored inside an object. 😊
--- 11_Objects/Practice.js
+++ 11_Objects/Practice.js
@@ -1,9 +1,20 @@
 // Creating Objects
 
-let person = {
-    name : "ali",
-    age : 23,
-    city : "Pirmahal"
-};
+// let person = {
+//     name : "ali",
+//     age : 23,
+//     city : "Pirmahal"
+// };
 
-console.log(person);
+// let person = new Object();
+// person.name = "Inshal";
+// person.age = 23;
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.city);
+// console.log(person["name"]);
+// console.log(person["age"]);
+// console.log(person["city"]);
+
+
