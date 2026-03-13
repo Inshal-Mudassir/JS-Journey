c4c2762c7386d75a7ead7259070ebfd97f2cc03e
Inshal <inshalmudassar715@gmail.com>
2026-03-13 21:52:38 +0500

    Creating Objects

---
+++ 11_Objects/1_CreatingObjects.js
@@ -0,0 +1,30 @@
+// Creating Objects — JavaScript (Very Easy 😊)
+// An object stores data in key–value pairs.
+
+// 🔹 1️⃣ Object Literal (Most Common)
+// let person = {
+//   name: "Ali",
+//   age: 20,
+//   city: "Lahore"
+// };
+
+// console.log(person);
+// 👉 name, age, city = keys
+// 👉 "Ali", 20, "Lahore" = values
+
+// 🔹 2️⃣ Using new Object()
+// let person = new Object();
+
+// person.name = "Ali";
+// person.age = 20;
+
+// console.log(person);
+// 🔑 Easy Rule
+// Object = collection of key : value pairs.
+
+// Example:
+
+// {
+//  name: "Ali",
+//  age: 20
+// }
---
+++ 11_Objects/Practice.js
@@ -0,0 +1,9 @@
+// Creating Objects
+
+let person = {
+    name : "ali",
+    age : 23,
+    city : "Pirmahal"
+};
+
+console.log(person);
