f2437f408517e299390211f85c1f051632b7f558
Inshal <inshalmudassar715@gmail.com>
2026-03-14 14:23:36 +0500

    Object.values()

---
+++ 11_Objects/10_Object.values().js
@@ -0,0 +1,20 @@
+// Object.values() — JavaScript 😊
+// Object.values() is used to get all the values of an object.
+
+// 🔹 Example
+// let person = {
+//   name: "Ali",
+//   age: 20,
+//   city: "Lahore"
+// };
+
+// let values = Object.values(person);
+
+// console.log(values);
+// Output
+
+// ["Ali", 20, "Lahore"]
+// 👉 It returns an array of object values.
+
+// 🔑 Easy Rule
+// Object.values(object) → returns all values of the object as an array.
--- 11_Objects/Practice.js
+++ 11_Objects/Practice.js
@@ -88,3 +88,6 @@
 // };
 // let keys = Object.keys(person);
 // console.log(keys);
+
+// let values = Object.values(person);
+// console.log(values);
