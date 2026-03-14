929928aa81d1292532064298a8d40ba28efcd398
Inshal <inshalmudassar715@gmail.com>
2026-03-14 14:46:53 +0500

    String Methods

---
+++ 12_Strings & Numbers/2_StringMethods.js
@@ -0,0 +1,45 @@
+// String Methods — JavaScript (Very Easy 😊)
+// String methods are functions used to work with text.
+
+// 🔹 length → Count characters
+// let text = "Hello";
+
+// console.log(text.length);
+// Output
+
+// 5
+// 🔹 toUpperCase() → Capital letters
+// let text = "hello";
+
+// console.log(text.toUpperCase());
+// Output
+
+// HELLO
+// 🔹 toLowerCase() → Small letters
+// let text = "HELLO";
+
+// console.log(text.toLowerCase());
+// Output
+
+// hello
+// 🔹 includes() → Check text exists
+// let text = "Hello World";
+
+// console.log(text.includes("World"));
+// Output
+
+// true
+// 🔹 indexOf() → Find position
+// let text = "Hello";
+
+// console.log(text.indexOf("e"));
+// Output
+
+// 1
+// 🔑 Quick Summary
+// Method	Purpose
+// length	count characters
+// toUpperCase()	capital letters
+// toLowerCase()	small letters
+// includes()	check text
+// indexOf()	find position
---
+++ 12_Strings & Numbers/Practice.js
@@ -0,0 +1,6 @@
+// let userName = "inshal";
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+// console.log(userName.includes("al"));
+// console.log(userName.indexOf("al"));
