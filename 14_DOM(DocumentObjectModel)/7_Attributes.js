337152323f06e1a844b65f622dfe49e065cd7a9b
Inshal <inshalmudassar715@gmail.com>
2026-03-18 21:11:53 +0500

    Attributes

---
+++ 14_DOM(DocumentObjectModel)/7_Attributes.js
@@ -0,0 +1,21 @@
+// Attributes — JavaScript (Very Easy 😊)
+// Attributes are extra information added to HTML elements (like id, class, src, etc.).
+
+// 🔹 Example HTML
+// <img src="image.jpg" id="pic" />
+// 👉 src and id are attributes
+
+// 🔹 Get Attribute
+// let img = document.querySelector("img");
+
+// console.log(img.getAttribute("src"));
+// 👉 Gets value of src
+
+// 🔹 Set Attribute
+// img.setAttribute("src", "new.jpg");
+// 👉 Changes attribute value
+
+// 🔹 Remove Attribute
+// img.removeAttribute("id");
+// 🔑 Easy Rule
+// Attributes = extra details of HTML elements that JavaScript can get, set, or remove. 😊
