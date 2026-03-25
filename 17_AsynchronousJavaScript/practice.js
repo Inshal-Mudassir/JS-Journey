3dd00240163b1413858f43cfebe8e17bf14d3372
Inshal <inshalmudassar715@gmail.com>
2026-03-25 14:30:28 +0500

    Async&Await Done

--- 17_AsynchronousJavaScript/practice.js
+++ 17_AsynchronousJavaScript/practice.js
@@ -1,73 +1,106 @@
-// Promise 
+// // Promise 
 
-let myPromise = new Promise((resolve,reject)=>{
-    let success = true;
-    if (success) {
-        resolve("Task Completed!");
-    } else {
-        reject("Task Failed!");
-    }
-});
+// let myPromise = new Promise((resolve,reject)=>{
+//     let success = true;
+//     if (success) {
+//         resolve("Task Completed!");
+//     } else {
+//         reject("Task Failed!");
+//     }
+// });
 
-// Use of .then & .catch
+// // Use of .then & .catch
 
-myPromise
-.then((result)=>{
+// myPromise
+// .then((result)=>{
+//     console.log(result);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// });
+
+// // Real Example
+
+// function async() {
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("Task Done");
+//         },2000)
+//     });
+// }
+
+// async().then((data)=>{
+//     console.log(data);
+// }).catch((error)=>{
+//     console.log(error);
+// });
+
+// // Promise Chain
+
+// function step1() {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//         resolve(10)
+//         },2000);
+//     });
+// }
+
+// function step2(num) {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve(num * 2);
+//         },2000);
+//     });
+// }
+
+// step1()
+// .then((result)=>{
+//     return step2(result);
+// })
+// .then(final =>{
+//     console.log(final);
+// });
+
+// // Challenge
+
+// let greet = new Promise((resolve)=>{
+//     setTimeout(()=>{
+//         resolve("Hello Inshal");
+//     },2000);
+// });
+
+// greet
+// .then(res=> console.log(res));
+
+// Async Function
+
+// async function greet() {
+//     return "Hello Inshal";
+// }
+// greet()
+// .then(res => console.log(res));
+
+// await
+
+// async function users() {
+//     let res = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await res.json();
+
+//     console.log(data);
+// }
+
+// users();
+
+function delay (){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Done");
+        },2000);
+    });
+}
+
+async function run (){
+    let result = await delay();
     console.log(result);
-})
-.catch((error)=>{
-    console.log(error);
-});
-
-// Real Example
-
-function async() {
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve("Task Done");
-        },2000)
-    });
 }
-
-async().then((data)=>{
-    console.log(data);
-}).catch((error)=>{
-    console.log(error);
-});
-
-// Promise Chain
-
-function step1() {
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-        resolve(10)
-        },2000);
-    });
-}
-
-function step2(num) {
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve(num * 2);
-        },2000);
-    });
-}
-
-step1()
-.then((result)=>{
-    return step2(result);
-})
-.then(final =>{
-    console.log(final);
-});
-
-// Challenge
-
-let greet = new Promise((resolve)=>{
-    setTimeout(()=>{
-        resolve("Hello Inshal");
-    },2000);
-});
-
-greet
-.then(res=> console.log(res));
+run();
