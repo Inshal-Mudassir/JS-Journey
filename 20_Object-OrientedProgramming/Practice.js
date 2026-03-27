88d7625fc85512265514edd5994e8f153d92be4d
Inshal <inshalmudassar715@gmail.com>
2026-03-27 19:35:13 +0500

    Constructor

--- 20_Object-OrientedProgramming/Practice.js
+++ 20_Object-OrientedProgramming/Practice.js
@@ -21,4 +21,42 @@
 // console.log(p1.name);
 // console.log(p1.age);
 
+// class User {
+//     constructor(name = "guest"){
+//         this.name = name;
+//     }
+// }
 
+// let u1 = new User("Inshal");
+// console.log(u1.name);
+
+// Real Life Example
+
+// class BankAccount {
+//     constructor(name, balance) {
+//         this.name = name;
+//         this.balance = balance;
+//     }
+//     deposit(amount) {
+//         this.balance += amount;
+//     }
+// }
+
+// let acc = new BankAccount("Inshal", 3000);
+// acc.deposit(2000);
+// console.log(acc.balance);
+
+// Challenge Question
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    details(){
+        console.log(`Product Name : ${this.name} , Price : ${this.price}`);
+    }
+}
+
+let p1 = new Product("Apple", 25);
+p1.details();
