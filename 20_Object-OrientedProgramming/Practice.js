0241f498f231e1356dd803759bb1501b448758d3
Inshal <inshalmudassar715@gmail.com>
2026-03-27 19:25:24 +0500

    Classes Done

---
+++ 20_Object-OrientedProgramming/Practice.js
@@ -0,0 +1,24 @@
+// // Classes 
+
+// class Person {
+//     // Constructor
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greet() {
+//         console.log(`Hello ${this.name}`)
+//     }
+// }
+
+// // Object
+
+// let p1 = new Person("Inshal", 21);
+// let p2 = new Person("Ali", 31);
+
+// p1.greet();
+// p2.greet();
+// console.log(p1.name);
+// console.log(p1.age);
+
+
