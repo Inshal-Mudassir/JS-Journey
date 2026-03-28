73a853e7035176b2e847030c7e37813dcb7068a4
Inshal <inshalmudassar715@gmail.com>
2026-03-28 16:26:29 +0500

    Prototype Chain

---
+++ 21_Prototypes&Inheritance/Practice.js
@@ -0,0 +1,41 @@
+// // Custom Prototype
+
+// let animal = {
+//     eats : true
+// };
+// let dog = {
+//     barks : true
+// };
+// dog.__proto__ = animal;
+// console.log(dog.eats);
+
+// // Multiple Chain Level
+
+// let grandparent = {
+//     money : true
+// };
+// let parent = {
+//     house : true
+// }
+// let child = {
+//     bike : true
+// }
+
+// parent.__proto__ = grandparent;
+// child.__proto__ = parent;
+
+// console.log(child.money);
+
+// With Constructor Function 
+
+// function Person(name) {
+//     this.name = name;
+// }
+// Person.prototype.greet = function() {
+//     console.log(`Hello ${this.name}`);
+// }
+
+// let p1 = new Person("Inshal");
+// p1.greet();
+
+
