0970fcb77ebf022dc0b26e65d899a012448f0582
Inshal <inshalmudassar715@gmail.com>
2026-04-29 19:15:52 +0500

    console.table

---
+++ 3_JsBasic/0_Practice.js
@@ -0,0 +1,8 @@
+const accountId = 1444235;
+let accountEmail = "inshal@gmail.com";
+var accountPassword = "12345";
+accountCity = "Inshal";
+
+// accountId = 2;
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
