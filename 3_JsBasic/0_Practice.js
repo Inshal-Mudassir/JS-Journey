8bb4dddd1a18843aceaa6d08ed34adca51545ce7
Inshal <inshalmudassar715@gmail.com>
2026-04-29 19:32:19 +0500

    JS Basic -hitesh

--- 3_JsBasic/0_Practice.js
+++ 3_JsBasic/0_Practice.js
@@ -1,8 +1,9 @@
-const accountId = 1444235;
-let accountEmail = "inshal@gmail.com";
-var accountPassword = "12345";
-accountCity = "Inshal";
+// const accountId = 1444235;
+// let accountEmail = "inshal@gmail.com";
+// var accountPassword = "12345";
+// accountCity = "Inshal";
 
-// accountId = 2;
+// // accountId = 2;
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
+
